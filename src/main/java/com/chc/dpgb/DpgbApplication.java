be09310a5e5e6af858d9815a03b7abce6feb6bf0
changbill <changbill@naver.com>
2026-08-17 18:24:30 +0900

    first commit

---
+++ src/main/java/com/chc/dpgb/DpgbApplication.java
@@ -0,0 +1,13 @@
+package com.chc.dpgb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DpgbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DpgbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chc/dpgb/DpgbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chc.dpgb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DpgbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
